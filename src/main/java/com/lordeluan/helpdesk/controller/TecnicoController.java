f7e1ce2d484e9404275a2a0b25564414d46ee8ac
Luan Lima <lordellu@hotmail.com>
2022-05-16 13:26:28 -0300

    padrão dto para tecnico

--- src/main/java/com/lordeluan/helpdesk/controller/TecnicoController.java
+++ src/main/java/com/lordeluan/helpdesk/controller/TecnicoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lordeluan.helpdesk.dtos.TecnicoDTO;
 import com.lordeluan.helpdesk.entity.Tecnico;
 import com.lordeluan.helpdesk.service.TecnicoService;
 
@@ -18,8 +19,8 @@ public class TecnicoController {
 	private TecnicoService service;
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id){
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
 		Tecnico obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
 }
