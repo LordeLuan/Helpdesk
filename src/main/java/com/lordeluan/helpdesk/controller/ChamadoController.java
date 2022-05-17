797dd559d831ebefa3d3d786c4ca8d6e31e3f8a8
Luan Lima <lordellu@hotmail.com>
2022-05-17 10:55:22 -0300

    criado metodo create para o chamado

--- src/main/java/com/lordeluan/helpdesk/controller/ChamadoController.java
+++ src/main/java/com/lordeluan/helpdesk/controller/ChamadoController.java
@@ -1,13 +1,19 @@
 package com.lordeluan.helpdesk.controller;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lordeluan.helpdesk.dtos.ChamadoDTO;
 import com.lordeluan.helpdesk.entity.Chamado;
@@ -20,14 +26,23 @@ public class ChamadoController {
 	@Autowired
 	private ChamadoService service;
 	
+	@GetMapping
+	public ResponseEntity<List<ChamadoDTO>> findAll(){
+		 return ResponseEntity.ok().body(service.findAll());
+	}
+	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<ChamadoDTO> finddById(@PathVariable Integer id){
 		Chamado obj = service.findById(id);
 		return ResponseEntity.ok().body(new ChamadoDTO(obj));
 	}
 	
-	@GetMapping
-	public ResponseEntity<List<ChamadoDTO>> findAll(){
-		 return ResponseEntity.ok().body(service.findAll());
+	@PostMapping
+	public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO chamadoDTO){
+		Chamado obj = service.create(chamadoDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
+	
+
 }
--- src/main/java/com/lordeluan/helpdesk/service/ChamadoService.java
+++ src/main/java/com/lordeluan/helpdesk/service/ChamadoService.java
@@ -4,11 +4,17 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lordeluan.helpdesk.dtos.ChamadoDTO;
 import com.lordeluan.helpdesk.entity.Chamado;
+import com.lordeluan.helpdesk.entity.Cliente;
+import com.lordeluan.helpdesk.entity.Tecnico;
+import com.lordeluan.helpdesk.entity.enums.Prioridade;
+import com.lordeluan.helpdesk.entity.enums.Status;
 import com.lordeluan.helpdesk.repository.ChamadoRepository;
 import com.lordeluan.helpdesk.service.exceptions.ObjectnotFoundException;
 
@@ -18,6 +24,11 @@ public class ChamadoService {
 	
 	@Autowired
 	private ChamadoRepository repository;
+	
+	@Autowired
+	private TecnicoService tecnicoService;
+	@Autowired
+	private ClienteService clienteService;
 	
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
@@ -29,6 +40,30 @@ public class ChamadoService {
 		List<ChamadoDTO> listDTO = listChamado.stream().map( x -> new ChamadoDTO(x)).collect(Collectors.toList());
 		return listDTO;
 	}
+
+	public Chamado create(@Valid ChamadoDTO chamadoDTO) {
+		return repository.save(newChamado(chamadoDTO));
+	}
 	
+	private Chamado newChamado(ChamadoDTO obj) {
+		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+		Cliente cliente = clienteService.findById(obj.getCliente());
+		
+//		Verifica se o id do chamado é nulo / se ele já existe
+		Chamado chamado = new Chamado();
+		if(obj.getId() != null) {
+			chamado.setId(obj.getId());
+		}
+		
+//		Seta as informações para o novo chamado entidade ser inserido no banco
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+		chamado.setStatus(Status.toEnum(obj.getStatus()));
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoes(obj.getObservacoes());
+		
+		return chamado;
+	}
 	
 }
