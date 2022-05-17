c7916333f00bac21e81522f35fe298b43ae8fc0e
Luan Lima <lordellu@hotmail.com>
2022-05-17 10:33:18 -0300

    findById criado para chamados

---
+++ src/main/java/com/lordeluan/helpdesk/controller/ChamadoController.java
@@ -0,0 +1,26 @@
+package com.lordeluan.helpdesk.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lordeluan.helpdesk.dtos.ChamadoDTO;
+import com.lordeluan.helpdesk.entity.Chamado;
+import com.lordeluan.helpdesk.service.ChamadoService;
+
+@RestController
+@RequestMapping(value = "/chamados")
+public class ChamadoController {
+
+	@Autowired
+	private ChamadoService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ChamadoDTO> finddById(@PathVariable Integer id){
+		Chamado obj = service.findById(id);
+		return ResponseEntity.ok().body(new ChamadoDTO(obj));
+	}
+}
